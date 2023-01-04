1319d71943a90ec3322bd2445d4ef63fc4046c76
Ghost_chu <ghostchu@111.com>
2023-01-04 19:19:23 +0800

    修正传参问题

--- src/main/java/com/ghostchu/crowdincopydeploy/Main.java
+++ src/main/java/com/ghostchu/crowdincopydeploy/Main.java
@@ -1,9 +1,8 @@
 package com.ghostchu.crowdincopydeploy;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.Arrays;
 
 public class Main {
     private static final Logger LOG = LoggerFactory.getLogger(Main.class);
@@ -12,9 +11,10 @@ public class Main {
         LOG.info("CrowdinCopyDeploy v1.0.0");
         LOG.info("Starting up...");
         new CrowdinDeployer(new CrowdinAPI());
-        LOG.info("All completed");
-        if(Arrays.stream(args).anyMatch(s->s.equalsIgnoreCase("uploads3"))){
+        if(!StringUtils.isEmpty(System.getenv("AWS_S3_BUCKET"))){
+            LOG.info("S3 uploading...");
             new S3Uploader();
         }
+        LOG.info("All completed");
     }
 }
